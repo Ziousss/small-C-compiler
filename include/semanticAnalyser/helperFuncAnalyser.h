d677afa5226274307ad6bfac38787ceeced61ff6
Ziousss <johnsson_daniel@outlook.com>
2026-01-14 12:13:58 +0000

    Created the first helper function. they will act on the scope of variables and the symbol table

--- include/semanticAnalyser/helperFuncAnalyser.h
+++ include/semanticAnalyser/helperFuncAnalyser.h
@@ -1,10 +1,16 @@
-#ifndef NODEANALYSER_H
-#define NODEANALYSER_H
+#ifndef HERLPERFUNCANALYSER_H
+#define HERLPERFUNCANALYSER_H
 
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdbool.h>
 #include "../include/parser/ast.h"
+#include "struct.h"
+
+void push_to_scope(symbolNode *sym);
+void pop_out_scope(int count);
+symbolNode *find_in_scope(char *name);
+semanticType fromTokToSem(Tokentype type);
 
 #endif
--- source/semanticAnalyser/helperFuncAnalyser.c
+++ source/semanticAnalyser/helperFuncAnalyser.c
@@ -1,2 +1,39 @@
 #include "../include/semanticAnalyser/helperFuncAnalyser.h"
 
+static symbolNode *stack_scope = NULL;
+
+void push_to_scope(symbolNode *sym){
+    sym->next = stack_scope;
+    stack_scope = sym;
+}
+
+void pop_out_scope(int count){
+    while(stack_scope != NULL && count-- > 0){
+        symbolNode *tmp = stack_scope;
+        stack_scope = stack_scope->next;
+        free(tmp->name);
+        free(tmp);
+    }
+}
+
+symbolNode *find_in_scope(char *name){
+    symbolNode *cur = stack_scope;
+    while(cur){
+        if(strcmp(cur->name, name) == 0){
+            return cur;
+        }
+
+        cur = cur->next;
+    }
+    return NULL;
+}
+
+semanticType fromTokToSem(Tokentype type){
+    switch (type){
+        case TOK_CHAR:          return SEM_CHAR;
+        case TOK_INT:           return SEM_INT;
+        case TOK_VOID:          return SEM_VOID;
+        
+        default:                return SEM_ERROR;
+    }
+}
